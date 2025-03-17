68c9642bdd89272fc8e65e8fbbb70541cd5bb5dd
Tuna Kirkoc <tuna@kirkoc.fr>
2025-03-17 10:11:13 +0100

    Update BlueMap SQL configuration for MariaDB support

--- minecraft/bluemap/entrypoint.sh
+++ minecraft/bluemap/entrypoint.sh
@@ -5,4 +5,14 @@ if [ -n "$BLUEMAP_DB_URL" ]; then
     sed -i "s#connection-url: \"\"#connection-url: \"$escaped_url\"#" /app/config/storages/sql.conf
 fi
 
+if [ -n "$BLUEMAP_DB_USER" ]; then
+    escaped_user=$(printf '%s\n' "$BLUEMAP_DB_USER" | sed 's/[\/&]/\\&/g')
+    sed -i "s/{{DB_USER}}/$escaped_user/" /app/config/storages/sql.conf
+fi
+
+if [ -n "$BLUEMAP_DB_PASSWORD" ]; then
+    escaped_password=$(printf '%s\n' "$BLUEMAP_DB_PASSWORD" | sed 's/[\/&]/\\&/g')
+    sed -i "s/{{DB_PASSWORD}}/$escaped_password/" /app/config/storages/sql.conf
+fi
+
 exec java -jar cli.jar -r -w
